26a3da1e5b0e9f68b7eb72de8ff07bdfc887a485
parienko_sergej <parienko_sergej@enot.work>
2025-03-15 17:41:41 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,57 +1,55 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    reactStrictMode: false,
-    webpack(config) {
-        const fileLoaderRule = config.module.rules.find((rule) =>
-            rule.test?.test?.('.svg')
-        )
+  reactStrictMode: false,
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
-        config.module.rules.push(
-            {
-                ...fileLoaderRule,
-                test: /\.svg$/i,
-                resourceQuery: /url/,
-            },
-            {
-                test: /\.svg$/i,
-                issuer: fileLoaderRule.issuer,
-                resourceQuery: {
-                    not: [...fileLoaderRule.resourceQuery.not, /url/],
-                },
-                use: [
-                    {
-                        loader: '@svgr/webpack',
-                        options: {
-                            svgoConfig: {
-                                plugins: [
-                                    {
-                                        name: 'preset-default',
-                                        params: {
-                                            overrides: {
-                                                cleanupIds: false,
-                                                removeViewBox: false,
-                                            },
-                                        },
-                                    },
-                                    'removeXMLNS',
-                                ],
-                            },
-                        },
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...fileLoaderRule.resourceQuery.not, /url/],
+        },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        cleanupIds: false,
+                        removeViewBox: false,
+                      },
                     },
+                  },
+                  'removeXMLNS',
                 ],
-            }
-        )
+              },
+            },
+          },
+        ],
+      },
+    );
 
-        fileLoaderRule.exclude = /\.svg$/i
+    fileLoaderRule.exclude = /\.svg$/i;
 
-        return config
-    },
-    sassOptions: {
-        additionalData: `
-    @use "src/assets/styles/mixins.scss as *";
+    return config;
+  },
+  sassOptions: {
+    additionalData: `
+    @use "/src/assets/styles/mixins.scss";
   `,
-    },
-}
+  },
+};
 
-export default nextConfig
+export default nextConfig;
