1b729ccf45592d754c5ada4a39b78ef9a0d282ed
parienko_sergej <parienko_sergej@enot.work>
2025-03-15 17:43:21 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { MainPage } from '@/pageComponents/MainPage/MainPage';
+
 export default function Main() {
-  return (
-    <main className={'main'}>
-      <div>main</div>
-    </main>
-  );
+  return <MainPage />;
 }
---
+++ src/pageComponents/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './MainPage.module.scss'
+
+export const MainPage = () => {
+  return (
+    <main className={styles.root}>MainPage</main>
+  )
+}
