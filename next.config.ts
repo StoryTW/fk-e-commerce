7dc734a64a333023eb77cd5d981398bef79710a7
parienko_sergej <parienko_sergej@enot.work>
2025-03-15 17:35:12 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,57 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    reactStrictMode: false,
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.('.svg')
+        )
 
-export default nextConfig;
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: {
+                    not: [...fileLoaderRule.resourceQuery.not, /url/],
+                },
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            svgoConfig: {
+                                plugins: [
+                                    {
+                                        name: 'preset-default',
+                                        params: {
+                                            overrides: {
+                                                cleanupIds: false,
+                                                removeViewBox: false,
+                                            },
+                                        },
+                                    },
+                                    'removeXMLNS',
+                                ],
+                            },
+                        },
+                    },
+                ],
+            }
+        )
+
+        fileLoaderRule.exclude = /\.svg$/i
+
+        return config
+    },
+    sassOptions: {
+        additionalData: `
+    @use "src/assets/styles/mixins.scss as *";
+  `,
+    },
+}
+
+export default nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import '@/assets/styles/index.scss';
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -23,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ru">
+      <body className={`${geistMono.variable}`}>
         {children}
       </body>
     </html>
