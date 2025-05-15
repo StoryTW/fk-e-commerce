4f11b35fce7be6c765658ae9758e1965ce6b6c0a
parienko_sergej <parienko_sergej@enot.work>
2025-05-15 18:19:58 +0300

    fix

--- src/sections/CatalogList/GameCard/RightSide/RightSide.tsx
+++ src/sections/CatalogList/GameCard/RightSide/RightSide.tsx
@@ -3,19 +3,26 @@ import styles from './RightSide.module.scss';
 import { motion } from 'motion/react';
 import { animateHelper } from '@/utils/helpers/animation.helper';
 import { Button } from '@/components/ui/Buttons/Button/Button';
-
-const data = {
-  genre: 'Стратегия',
-  locale: 'Русский(интерфейс, субтитры)',
-};
+import { useCartStore } from '@/store/useCartStore';
 
 interface IRightSide {
   data: GameModel;
 }
 
 export const RightSide = ({ data }: IRightSide) => {
-  const handleAddToCart = () => {
-    console.log('add');
+  const addItem = useCartStore((state) => state.addItem);
+
+  const handleAddToCart = (
+    e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>,
+    item: GameModel,
+  ) => {
+    e.preventDefault();
+    addItem({
+      id: String(item.id),
+      image: item.preview,
+      price: String(item.price),
+      title: item.title,
+    });
   };
 
   return (
@@ -40,7 +47,7 @@ export const RightSide = ({ data }: IRightSide) => {
           <div className={styles.valueText}>PS4 / PS5</div>
         </div>
 
-        <Button variant='primary' size='l' onClick={handleAddToCart}>
+        <Button variant='primary' size='l' onClick={(e) => handleAddToCart(e, data)}>
           Добавить в корзину
         </Button>
       </div>
